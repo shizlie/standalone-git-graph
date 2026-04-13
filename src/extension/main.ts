530d1edd7b926907c3a0510e7522a8393a00618e
asispts <79239132+asispts@users.noreply.github.com>
2026-04-13 08:20:26 +0000

    Modify activate function to register the ext commands

--- src/extension/main.ts
+++ src/extension/main.ts
@@ -2,19 +2,38 @@ import * as vscode from "vscode";
 
 import { findGitRepos } from "@/backend/queries/repoSearch";
 import { config } from "@/config";
-import { bootstrap } from "@/extension/bootstrap";
 import { waitForRepo } from "@/extension/waitForRepo";
+import * as l10n from "@/l10n";
 
 export async function activate(ctx: vscode.ExtensionContext) {
-  const paths = (vscode.workspace.workspaceFolders ?? []).map((f) => f.uri.fsPath);
+  l10n.initL10n(ctx.extensionUri.fsPath);
 
+  const paths = (vscode.workspace.workspaceFolders ?? []).map((f) => f.uri.fsPath);
   const repoDirs = await findGitRepos(paths, config.gitPath(), config.maxDepthOfRepoSearch());
-  if (repoDirs.length <= 0) {
-    ctx.subscriptions.push(waitForRepo(ctx));
+
+  if (repoDirs.length > 0) {
+    // bootstrap(ctx, repoDirs);
     return;
   }
 
-  bootstrap(ctx, repoDirs);
+  ctx.subscriptions.push(waitForRepo(ctx));
+
+  // Commands fallback
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("neo-git-graph.view", async () => {
+      await vscode.window.showErrorMessage(l10n.t("statusBar.text"), {
+        detail: l10n.t("error.noGitRepository"),
+        modal: true
+      });
+    }),
+
+    vscode.commands.registerCommand("neo-git-graph.clearAvatarCache", async () => {
+      await vscode.window.showErrorMessage(l10n.t("statusBar.text"), {
+        detail: l10n.t("error.noGitRepository"),
+        modal: true
+      });
+    })
+  );
 }
 
 export function deactivate() {}
